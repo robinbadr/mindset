97676212147c83a80cb9fba8f9cdd86b76791f52
Robin Badr <robinbadr@gmail.com>
2025-02-08 19:30:22 -0800

    changes

--- mindset-blocks/mindset-blocks.php
+++ mindset-blocks/mindset-blocks.php
@@ -16,6 +16,10 @@ function mindset_blocks_mindset_blocks_block_init() {
 		__DIR__ . '/build/service-posts',
 		array( 'render_callback' => 'fwd_render_service_posts' ) 
 	);
+	register_block_type(
+		__DIR__ . '/build/testimonial-slider',
+		array( 'render_callback' => 'fwd_render_testimonial_slider' ) 
+	);
 	
 }
 add_action( 'init', 'mindset_blocks_mindset_blocks_block_init' );
@@ -95,6 +99,50 @@ function fwd_render_service_posts( $attributes ) {
  
             echo '</section>';
         }
+        ?>
+    </div>
+    <?php
+    return ob_get_clean();
+}
+
+// Callback function for the Testimonial Slider
+function fwd_render_testimonial_slider( $attributes, $content ) {
+    ob_start();
+    $swiper_settings = array(
+        'pagination' => $attributes['pagination'],
+        'navigation' => $attributes['navigation']
+    );
+    ?>
+    <div <?php echo get_block_wrapper_attributes(); ?>>
+        <script>
+            const swiper_settings = <?php echo json_encode( $swiper_settings ); ?>;
+        </script>
+        <?php
+        $args = array(
+            'post_type'      => 'fwd-testimonial',
+            'posts_per_page' => -1
+        );
+        $query = new WP_Query( $args );
+        if ( $query->have_posts() ) : ?>
+            <div class="swiper">
+                <div class="swiper-wrapper">
+                    <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+                        <div class="swiper-slide">
+                            <?php the_content(); ?>
+                        </div>
+                    <?php endwhile; ?>
+                </div>
+            </div>
+            <?php if ( $attributes['pagination'] ) : ?>
+                <div class="swiper-pagination"></div>
+            <?php endif; ?>
+            <?php if ( $attributes['navigation'] ) : ?>
+                <button class="swiper-button-prev"></button>
+                <button class="swiper-button-next"></button>
+            <?php endif; ?>
+            <?php
+            wp_reset_postdata();
+        endif;
         ?>
     </div>
     <?php
---
+++ mindset-blocks/patterns/book-review.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Book Review
+ * Slug: mindset-theme/book-review
+ * Categories: media, text
+*/
+?>
+
+<!-- wp:media-text {"mediaId":216,"mediaLink":"http://localhost/mindset/works/photo-1/yellow-door/","mediaType":"image","mediaWidth":40,"mediaSizeSlug":"large"} -->
+<div class="wp-block-media-text is-stacked-on-mobile" style="grid-template-columns:40% auto"><figure class="wp-block-media-text__media"><img src="http://localhost/mindset/wp-content/uploads/2025/02/yellow-door-1024x682.jpg" alt="a yellow door" class="wp-image-216 size-large"/></figure><div class="wp-block-media-text__content"><!-- wp:heading -->
+<h2 class="wp-block-heading">Title</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Author</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Description</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
